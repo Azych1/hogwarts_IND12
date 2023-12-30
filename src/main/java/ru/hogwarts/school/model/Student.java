6ed0e4a42cf6930122aae439601ef30415c51be8
hw-2.15 <nagimov123@gmail.com>
2023-12-31 04:09:22 +0500

    Добавили классы/методы для работы с avatar

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -24,11 +24,11 @@ public class Student {
     public Student() {
     }
 
-    //    public Student(Long id, String name, int age) {
-//        this.id = id;
-//        this.name = name;
-//        this.age = age;
-//    }
+        public Student(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 
     public Student(String name, int age) {
         this.name = name;
